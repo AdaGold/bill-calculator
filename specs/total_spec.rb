cfcd13953ce90feae81039a5adfa0dd5bdd1f2c3
CheezItMan <chris@adadevelopersacademy.org>
2018-07-23 09:49:32 -0700

    gave example bill_items in total_spec.rb

--- specs/total_spec.rb
+++ specs/total_spec.rb
@@ -3,5 +3,19 @@
 require_relative '../lib/bill'
 
 describe "total" do
-  
+  it "will calculate the total resturant bill" do
+    # Arrange
+    bill_items = [
+      {item: "Cesar Salad", price: 31},
+      {item: "Tacos", price: 14},
+      {item: "Ice Tea", price: 2.95},
+      {item: "Coke", price: 0.99}
+    ]
+
+    # Act
+
+
+    # Assert
+    
+  end
 end
