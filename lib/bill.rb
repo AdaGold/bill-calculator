c1d3944cabda49404516da1e4aca2f252762e033
CheezItMan <chris@adadevelopersacademy.org>
2018-07-23 09:54:07 -0700

    added tax

--- lib/bill.rb
+++ lib/bill.rb
@@ -1,5 +1,14 @@
 # bill.rb
 
 def subtotal(prices)
+  if prices.class != Array
+    raise ArgumentError.new("subtotal requires an Array")
+  end
 
+  return prices.reduce(0, :+)
+end
+
+
+def tax(prices, tax_rate)
+  return subtotal(prices) * tax_rate
 end
--- specs/tax_spec.rb
+++ specs/tax_spec.rb
@@ -15,16 +15,30 @@ describe "tax" do
     sub = tax(prices, 0.08)
 
     # Assert
-
+    expect(sub).must_be_close_to 0.96, 0.01
   end
 
-  it "also works with floating point numbers" do
+  it "will not allow a negative tax rate" do
+    # Arrange
+    prices = [3, 4, 5]
+    tax_rate = - 0.01
 
-
+    # Act-Assert
+    expect {
+      sub = tax(prices, tax_rate)
+    }.must_raise ArgumentError
   end
 
   it "will return 0 for an empty array" do
 
+    # Arrange
+    prices = []
+
+    # Act
+    sub = tax(prices, 0.08)
+
+    # Assert
+    expect(sub).must_equal(0)
   end
 
   it "will raise an error for a non-array argument" do
