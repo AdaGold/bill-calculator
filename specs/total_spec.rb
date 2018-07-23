1f757b5590ab62193fffad2f50fa941a3cb31f89
CheezItMan <chris@adadevelopersacademy.org>
2018-07-23 10:06:41 -0700

    tested with a return (negative bill)

--- specs/total_spec.rb
+++ specs/total_spec.rb
@@ -30,4 +30,21 @@ describe "total" do
     # Assert
     expect(total_bill).must_equal 0
   end
+
+  it "includes 0 tax for a refund" do
+    # Arrange
+    bill_items = [
+      {item: "Cesar Salad", price: -31},
+      {item: "Tacos", price: -14},
+      {item: "Ice Tea", price: -2.95},
+      {item: "Coke", price: -0.99}
+    ]
+
+    # Act
+    total_bill = total(bill_items, 0.08)
+
+    # Assert
+    expect(total_bill).must_be_close_to -48.94, 0.01
+  end
+
 end
